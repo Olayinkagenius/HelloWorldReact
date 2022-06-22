17bd617e4e58827ecacf9e4d579dc9369bbc0529
Olayinka Adeyemi <olayinkaadeyemi343@gmail.com>
2022-06-22 12:16:32 +0100

    Finishing touches!

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,8 +4,8 @@ const Description = () => {
     <div className="description">
       This is my <span className="italic">First Application</span> using React
       JS. <br /> <br />
-      Although it is a very tiny app it still describes how to create components
-      in React, an awesome Javascript Library made by Facebook
+
+
     </div>
   )
 }
